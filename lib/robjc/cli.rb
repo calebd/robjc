3fa0c07d3dd16f51cc61477cfbdc3172289541d1
Caleb Davenport <calebmdavenport@gmail.com>
2014-03-05 19:21:12 -0800

    Add ability to watch a folder.

--- lib/robjc/cli.rb
+++ lib/robjc/cli.rb
@@ -1,16 +1,37 @@
 require 'robjc'
 require 'thor'
+require 'listen'
 
 module Resources
-  class Cli < thor
+  class Cli < Thor
 
-    desc 'generate'
+    desc 'generate', 'Generate resource files for the current directory.'
     def generate
       reader = Resources::Reader.new
       resources = reader.resources
 
       writer = Resources::Writer.new(resources)
       writer.write
+    end
+
+    desc 'watch', 'Watch the current folder for changes and update all resources automatically.'
+    def watch
+      path = File.expand_path('.')
+      puts "Listening to changes in #{path}"
+      listener = Listen.to(path) do |modified, added, removed|
+        puts "Generating resources"
+
+        reader = Resources::Reader.new
+        resources = reader.resources
+
+        writer = Resources::Writer.new(resources)
+        writer.write
+      end
+      listener.ignore /CMDResources/
+      listener.ignore /\.h$/
+      listener.ignore /\.m$/
+      listener.start
+      sleep
     end
 
   end
--- robjc.gemspec
+++ robjc.gemspec
@@ -18,10 +18,9 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  # spec.add_runtime_dependency "listen"
-  spec.add_runtime_dependency "xcodeproj"
   spec.add_runtime_dependency "plist"
   spec.add_runtime_dependency "thor"
+  spec.add_runtime_dependency "listen"
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
